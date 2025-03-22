cc8f159018211d1bc73e8febd2f334290ccbb9ce
EinsiTang <my-tangjianbin@163.com>
2025-03-22 15:30:43 +0800

    android & ios cgo build script fix

--- scripts/android_build.sh
+++ scripts/android_build.sh
@@ -5,10 +5,19 @@ android_sdk=/Users/einsitang/Library/Android/sdk/ndk/29.0.13113456
 
 platform="android"
 
+# x86_64 for simulator
+arch="amd64"
+export GOOS="android"
+export GOARCH="amd64"
+export CGO_ENABLED="1"
+export CC="$android_sdk/toolchains/llvm/prebuilt/darwin-x86_64/bin/x86_64-linux-android35-clang"
+export CXX="$android_sdk/toolchains/llvm/prebuilt/darwin-x86_64/bin/x86_64-linux-android35-clang++"
+go build -buildmode=c-shared -ldflags="-s -w" -o build/cgo/$platform/libsudoku_$arch.so ./cgo
+
 # aarch64
 arch="aarch64"
 export GOOS="android"
-export GOARCH="arm64"
+export GOARCH="arm"
 export CGO_ENABLED="1"
 export CC="$android_sdk/toolchains/llvm/prebuilt/darwin-x86_64/bin/aarch64-linux-android35-clang"
 export CXX="$android_sdk/toolchains/llvm/prebuilt/darwin-x86_64/bin/aarch64-linux-android35-clang++"
--- scripts/ios_build.sh
+++ scripts/ios_build.sh
@@ -10,26 +10,23 @@ export CGO_CFLAGS="-arch arm64 -isysroot $(xcrun --sdk iphoneos --show-sdk-path)
 go build -buildmode=c-archive -o build/cgo/ios/libsudoku_arm64.a ./cgo
 
 # 编译模拟器 (x86_64 / arm64)
-export GOARCH=amd64
-# export GOARCH=arm64
+export GOARCH=amd64 # <- x86_64 here
+# export GOARCH=arm64 # <- arm64 here
 export CGO_ENABLED=1
 export CC=$(xcrun --sdk iphonesimulator --find clang)
-export CGO_CFLAGS="-arch x86_64 -isysroot $(xcrun --sdk iphonesimulator --show-sdk-path)"
-# export CGO_LDFLAGS="-arch arm64 -isysroot $(xcrun --sdk iphonesimulator --show-sdk-path) -Wl,-install_name,@rpath/libexample.dylib"
+export CGO_CFLAGS="-arch x86_64 -isysroot $(xcrun --sdk iphonesimulator --show-sdk-path)" # <- x86_64 here
+# export CGO_LDFLAGS="-arch arm64 -isysroot $(xcrun --sdk iphonesimulator --show-sdk-path) -Wl,-install_name,@rpath/libexample.dylib" # <- arm64 here
 go build -buildmode=c-archive -o build/cgo/ios/libsudoku_simulator_$GOARCH.a ./cgo
 
-# 使用 xcodebuild 组合成xcframework
-# xcodebuild -create-xcframework -library build/cgo/ios/libsudoku_x86_64.a -library build/cgo/ios/libsudoku_arm64.a -output ./build/cgo/ios/libsudoku.xcframework
-
 # 使用lipo组合静态库 (相同平台不通架构：same-platform: ios-simulator, combo arch: x86_64, arm64)
-# lipo -create libsudoku_simulator_amd64.a libsudoku_simulator_arm64.a -output libsudoku_simulator_universal.a
+# lipo -create build/cgo/ios/libsudoku_simulator_amd64.a build/cgo/ios/libsudoku_simulator_arm64.a -output build/cgo/ios/libsudoku_simulator_universal.a
 
 # 查看
 # lipo -info libsudoku_simulator_universal.a
 
 # final build
 # 使用 xcodebuild 将不同平台(ios-simulator,ios) 合并成 xcframework
-# xcodebuild -create-xcframework -library libsudoku_arm64.a -library libsudoku_simulator_universal.a -output libsudoku.xcframework
+# xcodebuild -create-xcframework -library build/cgo/ios/libsudoku_arm64.a -library build/cgo/ios/libsudoku_simulator_universal.a -output build/cgo/ios/libsudoku.xcframework
 
 # 以下静态库转动态库方式 <- 未验证
 # !! Attention !! 
